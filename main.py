200dfa45563b8bb09cf529c188b19656eed05d66
VictorProgene <95300162+VictorProgene@users.noreply.github.com>
2026-04-07 12:18:15 -0300

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import datetime as dt
 import smtplib
 
-FROM_MY_EMAIL = "pythonclasses145@gmail.com"
-PASSWORD = "uueeblpivzcfxwvg"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 # from main import birthday
 
